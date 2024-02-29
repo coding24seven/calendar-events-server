c6587ad3198b53b9e0e3f0127397925efad6a8a5
AW <codingdayandnight@gmail.com>
2024-02-29 16:26:35 +0100

    improve handling of missing auth code

--- src/routes/redirect.ts
+++ src/routes/redirect.ts
@@ -5,7 +5,12 @@ import User from '../User'
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  const { code } = req.query
+  const code = req.query.code
+
+  if (!code) {
+    res.status(400).json({ error: 'authorization code missing' })
+  }
+
   const { tokens } = await oauth2Client.getToken(code as string)
 
   oauth2Client.setCredentials(tokens)
@@ -25,7 +30,7 @@ router.get('/', async (req, res) => {
   if (process.env.CLIENT_URI) {
     res.redirect(process.env.CLIENT_URI)
   } else {
-    res.status(500).send('client uri is not defined')
+    res.status(500).json({ error: 'client uri is not defined' })
   }
 })
 
